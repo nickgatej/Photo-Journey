c8b80dbeb46ae25127738b85d8e8fefe45153ae1
Nick <nick.gatej01@gmail.com>
2023-07-15 02:23:48 +0300

    Hot reloading and a very simple path handler func

--- main.go
+++ main.go
@@ -5,15 +5,48 @@ import (
 	"net/http"
 )
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_, err := fmt.Fprint(w, "<h1>Welcome!</h1>")
 	if err != nil {
 		return
 	}
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nick.gatej01@gmail.com\">nick.gatej01@gmail.com</a>.</p>")
+	if err != nil {
+		return
+	}
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// Handle the page not found error
+
+		// 1
+		w.WriteHeader(http.StatusNotFound)
+		_, err := fmt.Fprint(w, "Page not found")
+		if err != nil {
+			return
+		}
+
+		// 2
+		// http.Error(w, "The page you are looking for cannot be found!", http.StatusNotFound)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", requestHandler)
+	// http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/contact", contactHandler)
+
+	http.HandleFunc("/", pathHandler)
 
 	fmt.Println("Starting the server on :3000...")
 	err := http.ListenAndServe(":3000", nil)
