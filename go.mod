d822a1403a484929d75357eeb946dc7c9dca1277
Nick <nick.gatej01@gmail.com>
2023-07-15 02:49:00 +0300

    Using CHI Router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nickgatej/Photo-Journey
 
 go 1.17
+
+require github.com/go-chi/chi/v5 v5.0.7
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,35 +23,40 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		// Handle the page not found error
-
-		// 1
-		w.WriteHeader(http.StatusNotFound)
-		_, err := fmt.Fprint(w, "Page not found")
-		if err != nil {
-			return
-		}
-
-		// 2
-		// http.Error(w, "The page you are looking for cannot be found!", http.StatusNotFound)
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+	  <li>
+		<b>Is there a free version?</b>
+		Yes! We offer a free trial for 30 days on any paid plans.
+	  </li>
+	  <li>
+		<b>What are your support hours?</b>
+		We have support staff answering emails 24/7, though response
+		times may be a bit slower on weekends.
+	  </li>
+	  <li>
+		<b>How do I contact support?</b>
+		Email us - <a href="mailto:nick.gatej01@gmail.com">nick.gatej01@gmail.com</a>
+	  </li>
+	</ul>`)
+	if err != nil {
+		return
 	}
 }
 
 func main() {
-	// http.HandleFunc("/", homeHandler)
-	// http.HandleFunc("/contact", contactHandler)
-
-	http.HandleFunc("/", pathHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 
 	fmt.Println("Starting the server on :3000...")
-	err := http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		return
 	}
