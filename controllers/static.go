5375b5a224beb36ea15341d760ee62551f8103b8
Nick <nick.gatej01@gmail.com>
2023-07-16 01:28:22 +0300

    Creating Reusable Layout using Named Templates & Tailwind CSS

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/nickgatej/Photo-Journey/views"
@@ -17,5 +18,32 @@ func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML // This type is safe to be rendered, they are not user provided (no XSS)
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:nick.gatej01@gmail.com">nick.gatej01@gmail.com</a>`,
+		},
+		{
+			Question: "Where is your office?",
+			Answer:   "Our entire team is remote!",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
--- main.go
+++ main.go
@@ -12,17 +12,17 @@ import (
 func main() {
 	router := chi.NewRouter()
 
-	router.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
-	router.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
-	router.Get("/faq", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	router.Get("/", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+	router.Get("/contact", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+	router.Get("/faq", controllers.FAQ(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
 	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
-	
+
 	fmt.Println("Starting the server on :3000...")
 	err := http.ListenAndServe(":3000", router)
 	if err != nil {
