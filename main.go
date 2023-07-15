c9e85a95bd3e9fd584addd29fa46498dd4c62b91
Nick <nick.gatej01@gmail.com>
2023-07-15 17:45:49 +0300

    Server Side Rendering using Go Templates

--- main.go
+++ main.go
@@ -2,61 +2,54 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/go-chi/chi/v5"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, err := fmt.Fprint(w, "<h1>Welcome!</h1>")
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
+		log.Printf("Processing template: %v", err)
+		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
 		return
 	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, err := fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nick.gatej01@gmail.com\">nick.gatej01@gmail.com</a>.</p>")
-	if err != nil {
-		return
-	}
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, err := fmt.Fprint(w, `<h1>FAQ Page</h1>
-	<ul>
-	  <li>
-		<b>Is there a free version?</b>
-		Yes! We offer a free trial for 30 days on any paid plans.
-	  </li>
-	  <li>
-		<b>What are your support hours?</b>
-		We have support staff answering emails 24/7, though response
-		times may be a bit slower on weekends.
-	  </li>
-	  <li>
-		<b>How do I contact support?</b>
-		Email us - <a href="mailto:nick.gatej01@gmail.com">nick.gatej01@gmail.com</a>
-	  </li>
-	</ul>`)
-	if err != nil {
-		return
-	}
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func main() {
-	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+	router := chi.NewRouter()
+	router.Get("/", homeHandler)
+	router.Get("/contact", contactHandler)
+	router.Get("/faq", faqHandler)
+	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
 	fmt.Println("Starting the server on :3000...")
-	err := http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(":3000", router)
 	if err != nil {
 		return
 	}
