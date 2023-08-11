63f3c7b98293324228ad83c62458c00201d1d030
Nick <nick.gatej01@gmail.com>
2023-08-11 15:45:18 +0300

    Email Service & Forgot Password

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,9 +1,34 @@
-package exp
+package main
 
 import (
 	_ "github.com/jackc/pgx/v4/stdlib" // import with side effects
+	"github.com/joho/godotenv"
+	"github.com/nickgatej/Photo-Journey/models"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	host := os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		panic(err)
+	}
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+
+	es := models.NewEmailService(models.SMTPConfig{
+		Host:     host,
+		Port:     port,
+		Username: username,
+		Password: password,
+	})
+	es.ForgotPassword("nick.gatej1@gmail.com", "https://photojourney.com/reset-pw?token=abc123")
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/go-mail/mail/v2 v2.3.0 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.0 // indirect
@@ -19,6 +20,9 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
