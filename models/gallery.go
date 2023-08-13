5322ad6f0e08c54005f426e57ad39398639b2e3e
Nick <nick.gatej01@gmail.com>
2023-08-13 19:59:32 +0300

    Gallery Model & Gallery Service

---
+++ models/gallery.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Gallery struct {
+	ID     int
+	UserID int
+	Title  string
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+func (service *GalleryService) Create(title string, userID int) (*Gallery, error) {
+	gallery := Gallery{
+		Title:  title,
+		UserID: userID,
+	}
+	row := service.DB.QueryRow(`
+		INSERT INTO galleries (title, user_id)
+		VALUES ($1, $2) RETURNING id;`, gallery.Title, gallery.UserID)
+	err := row.Scan(&gallery.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create gallery: %w", err)
+	}
+	return &gallery, nil
+}
